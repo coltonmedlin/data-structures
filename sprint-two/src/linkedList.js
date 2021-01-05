14148bd5dd5825921836d649eca9043a15e6adf1
Colton Medlin <coltonmedlin@gmail.com>
2021-01-04 19:06:28 -0800

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    this.tail = node;
+    if (this.head === null) {
+      this.head = this.tail;
+    } else {
+      var current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+
   };
 
   list.removeHead = function() {
+    var value = this.head.value;
+    this.head = this.head.next;
+    return value;
   };
 
   list.contains = function(target) {
+    var current = this.head;
+    while (current.next !== null) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    // if (current.value === target) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return current.value === target ? true : false;
   };
 
   return list;
